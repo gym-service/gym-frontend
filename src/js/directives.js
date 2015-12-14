1cd5643bd88ab5c23b8d8c565e8e276e12ad80a5
mrjungle <davide.nuccio@gmail.com>
2015-12-15 00:29:48 +0100

    add state v1

--- src/js/directives.js
+++ src/js/directives.js
@@ -1,3 +1,14 @@
+.directive('app', [function () {
+    return {
+        restrict: 'A',
+        templateUrl : 'templates/menu.html',
+        replace : true,
+        link: function (scope, iElement, iAttrs) {
+            
+        }
+    };
+}])
+
 (function(){
 "use strict";
 
--- src/js/modules/config/states-config.js
+++ src/js/modules/config/states-config.js
@@ -72,6 +72,128 @@ angular.module('app.statesconfig', ['ui.router'])
         */
     })
 
+    .state('app.clublist', {
+    url: "/club",
+    url: "/club/:clubId",  
+    cache : true,
+    templateUrl: "templates/club.html",
+    controller : "ClubCtrl"  
+    })
+
+    .state('app.clubdetail', {
+    url: "/club/:clubId",  
+    cache : true,
+    templateUrl: "templates/clubdetail.html",
+    controller : "ClubDetailCtrl"
+    })
+
+    .state('app.servizilist', {
+    url: "/servizi",
+    cache : true,
+    templateUrl: "templates/service.html",
+    controller : "ServiziCtrl"  
+    })
+
+    .state('app.servizidetail', {
+    url: "/servizi",
+    cache : true,
+    templateUrl: "templates/servicedetail.html",
+    controller : "ServiziCtrl"  
+    })
+
+    .state('app.trainerslist', {
+    url: "/servizi/trainers",
+    cache : true,
+    templateUrl: "templates/trainer.html",
+    controller : "TrainersCtrl"
+    })
+
+    .state('app.trainerdetail', {
+    url: "/servizi/trainer/:trainerId",
+    cache : true,
+    templateUrl: "templates/servizidetail.html",
+    controller : "TrainerDetailCtrl"
+    })
+
+
+    .state('app.newslist', {
+    url: "/news",
+    cache : true,
+    templateUrl: "templates/trainer.html",
+    controller : "NewsCtrl"
+    })
+
+    .state('app.newsdetail', {
+    url: "/news/:trainerId",
+    cache : true,
+    templateUrl: "templates/newsdetail.html",
+    controller : "NewsDetailCtrl"
+    })
+
+    .state('app.presslist', {
+    url: "/press",
+    cache : true,
+    templateUrl: "templates/press.html",
+    controller : "PressCtrl"
+    })
+
+    .state('app.pressdetail', {
+    url: "/press/:pressId",
+    cache : true,
+    templateUrl: "templates/pressdetail.html",
+    controller : "PressDetailCtrl"
+    })
+
+    .state('app.eventilist', {
+    url: "/event",
+    cache : true,
+    templateUrl: "templates/eventi.html",
+    controller : "EventCtrl"
+    })
+
+    .state('app.eventidetail', {
+    url: "/eventi/:eventiId",
+    cache : true,
+    templateUrl: "templates/eventidetail.html",
+    controller : "EventiDetailCtrl"
+    })
+
+    .state('app.promozionilist', {
+    url: "/promozioni",
+    cache : true,
+    templateUrl: "templates/promozioni.html",
+    controller : "PromozioniCtrl"
+    })
+
+    .state('app.promozionidetail', {
+    url: "/promozioni/:promozioniId",
+    cache : true,
+    templateUrl: "templates/promozionidetail.html",
+    controller : "PromozioniDetailCtrl"
+    })
+
+    .state('app.contatti', {
+    url: "/contatti",
+    cache : false,
+    templateUrl: "templates/contatti.html",
+    controller : "ContattiCtrl"
+    })
+
+    .state('app.guestpass', {
+    url: "/guestpass",
+    cache : false,
+    templateUrl: "templates/guestpass.html",
+    controller : "GuestpassCtrl"
+    })
+
+    .state('app.orarilezioni', {
+    url: "/orarilezioni",
+    cache : false,
+    templateUrl: "templates/orarilezioni.html",
+    controller : "OrarilezioniCtrl"
+    })
+
+
     $urlRouterProvider.otherwise(function ($injector) {
         var $state = $injector.get('$state');
         $state.go('app.home');
