8a71f671ca0f7942d15dadadd18160b63f8a6057
Mauro Bianchi <bianchimro@gmail.com>
2015-12-16 12:27:24 +0100

    moved menu scroll logic to directive

--- src/js/directives/directive-menu.js
+++ src/js/directives/directive-menu.js
@@ -2,16 +2,55 @@
 "use strict";
 
 angular.module("app")
-.directive('appMenu', [function () {
+.directive('appMenu', ['$window', function ($window) {
     return {
         restrict: 'A',
         templateUrl : 'templates/menu.html',
         replace : true,
         link: function (scope, iElement, iAttrs) {
 
+            var docElem = document.documentElement,
+                navbar = $(iElement[0]),
+                didScroll = false,
+                changeHeaderOn = 160,
+                scrollListener = null;
+
+            function init() {
+                scrollListener = window.addEventListener( 'scroll', function() {
+                    if( !didScroll ) {
+                        didScroll = true;
+                        setTimeout( scrollPage, 250 );
+                    }
+                }, false );
+            }
+
+            function scrollPage() {
+                var sy = scrollY();
+                if ( sy >= changeHeaderOn ) {
+                    navbar.removeClass('navbar-shrink');
+                }
+                else {
+                    navbar.addClass('navbar-shrink');
+                }
+                didScroll = false;
+            }
+
+            function scrollY() {
+                return window.pageYOffset || docElem.scrollTop;
+            }
+
+            init();
+
+            scope.$on('$destroy', function(){
+                window.removeEventListener('scroll', scrollListener);
+            });
+
+
+
+
         }
     };
 }]);
-   
-    
+
+
 })();
